7f5e07a8da4aefa11c0d95552f2a0f0fa63b3cf7
jtheeeeee <j_03_17@naver.com>
2021-11-22 20:48:59 +0900

    init commit

---
+++ src/main/java/com/weling/we_are_traveling_java/WeAreTravelingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.weling.we_are_traveling_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeAreTravelingJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeAreTravelingJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weling/we_are_traveling_java/WeAreTravelingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weling.we_are_traveling_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeAreTravelingJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
