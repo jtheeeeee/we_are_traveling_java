1c8aca2ddb6769b7a42a5b0295fa42e497cb8a1d
김혜린 <37091602+burninghering@users.noreply.github.com>
2021-12-08 00:25:32 +0900

    Commuity 연결

--- src/main/java/com/weling/we_are_traveling_java/security/WebSecurityConfig.java
+++ src/main/java/com/weling/we_are_traveling_java/security/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
 
         http.authorizeRequests()
-                .antMatchers("/board/**").permitAll()
+                .antMatchers("/board").permitAll()
                 .antMatchers("/board_insert").permitAll()
                 .antMatchers("/view/**").permitAll()
                 .antMatchers("/images/**").permitAll()
