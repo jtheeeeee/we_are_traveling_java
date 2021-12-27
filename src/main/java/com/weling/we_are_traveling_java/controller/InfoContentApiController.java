8352b0ac4434b19c039cb3c9dee76ccfd23d6bc9
jtheeeeee <j_03_17@naver.com>
2021-12-28 01:46:08 +0900

    CORS에러 해결

--- src/main/java/com/weling/we_are_traveling_java/controller/InfoContentApiController.java
+++ src/main/java/com/weling/we_are_traveling_java/controller/InfoContentApiController.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@CrossOrigin(origins = "https://www.weling.site")
 public class InfoContentApiController {
 
     private final InfoContentService infoContentService;
